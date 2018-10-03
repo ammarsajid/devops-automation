d321166dc99e9c9a848dae98dd114ae1bd1c77ce
Ammar Sajid <ammar.sajid@techlogix.com>
2018-10-03 16:54:27 +0500

    Coded inter subscription cloning

--- powershell_scripts/db_cloning_automation/clone_db.ps1
+++ powershell_scripts/db_cloning_automation/clone_db.ps1
@@ -24,24 +24,22 @@ $ErrorActionPreference = "Stop"
 $curDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 $dbFilePath = $curDir + "\"+$dsub+ ".json"
-$dbFilePath = "D:\Techlogix\GitLab\Cloudops-09-25-18\cloudops\powershell_scripts\db_cloning_automation\an.json"
 $allDbsJson = ConvertFrom-Json "$(get-content $dbFilePath)"
 
 #Login to Azure Account
+
 # Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionID $allDbsJson.SubscriptionId
 
 if(-not($new))
 {
     # Storage account info to store BACPAC
-    # https://ancloudops.blob.core.windows.net/dbbackups2/BRAINS-prod-from-zain.bacpac
     $BaseStorageUri = "https://" + $allDbsJson.StorageAccName + ".blob.core.windows.net/" + $allDbsJson.StorageContainer + "/"
-    #$StorageKeytype = $allDbsJson.StorageKeytype
-    #$StorageKey = $allDbsJson.StorageKey
-    $bacpacFilename = $allDbsJson.Databases.$ddb.dbname + (Get-Date).ToString("yyyy-MM-dd-HH-mm") + ".bacpac"
-    $BacpacUri = $BaseStorageUri +"-"+ $bacpacFilename
-
+    $bacpacFilename = $allDbsJson.Databases.$ddb.dbname + "-" + (Get-Date).ToString("yyyy-MM-dd-HH-mm") + ".bacpac"
+    $BacpacUri = $BaseStorageUri + $bacpacFilename
+    $storageContextDest = New-AzureStorageContext -StorageAccountName $allDbsJson.StorageAccName -StorageAccountKey $allDbsJson.StorageKey
     # exporting db to bacpac
+    
     $exportRequest = New-AzureRmSqlDatabaseExport -ResourceGroupName $allDbsJson.Databases.$ddb.resourcegroup $allDbsJson.Databases.$ddb.server `
                         -DatabaseName $allDbsJson.Databases.$ddb.dbname -StorageKeytype $allDbsJson.StorageKeytype -StorageKey $allDbsJson.StorageKey `
                         -StorageUri $BacpacUri -AdministratorLogin $allDbsJson.Server_credentials.($allDbsJson.Databases.$ddb.server).username `
@@ -70,12 +68,64 @@ if(-not($backupOnly))
     Write-Host "Copying database to destination" -ForegroundColor Yellow
 
     # Copy source database to the target server
+    
+    if($ssub)
+    {
+        $dbFilePathSrc = $curDir + "\"+$ssub+ ".json"
+        $allDbsJsonSrc = ConvertFrom-Json "$(get-content $dbFilePathSrc)"
+        Select-AzureRmSubscription -SubscriptionID $allDbsJsonSrc.SubscriptionId
+        
+        # Storage account info to store BACPAC
+        $BaseStorageUriSrc = "https://" + $allDbsJsonSrc.StorageAccName + ".blob.core.windows.net/" + $allDbsJsonSrc.StorageContainer + "/"
+        $bacpacFilenameSrc = $allDbsJsonSrc.Databases.$sdb.dbname + "-" + (Get-Date).ToString("yyyy-MM-dd-HH-mm") + ".bacpac"
+        $BacpacUriSrc = $BaseStorageUriSrc + $bacpacFilenameSrc
+        $storageContextSrc = New-AzureStorageContext -StorageAccountName $allDbsJsonSrc.StorageAccName -StorageAccountKey $allDbsJsonSrc.StorageKey
+       
+        # exporting db to bacpac
+        $exportRequest = New-AzureRmSqlDatabaseExport -ResourceGroupName $allDbsJsonSrc.Databases.$sdb.resourcegroup $allDbsJsonSrc.Databases.$sdb.server `
+                            -DatabaseName $allDbsJsonSrc.Databases.$sdb.dbname -StorageKeytype $allDbsJsonSrc.StorageKeytype -StorageKey $allDbsJsonSrc.StorageKey `
+                            -StorageUri $BacpacUriSrc -AdministratorLogin $allDbsJsonSrc.Server_credentials.($allDbsJsonSrc.Databases.$sdb.server).username `
+                            -AdministratorLoginPassword ($allDbsJsonSrc.Server_credentials.($allDbsJsonSrc.Databases.$sdb.server).password | ConvertTo-SecureString -AsPlainText -Force )
+
+        Write-Host "Backup of" $allDbsJsonSrc.Databases.$sdb.dbname "is in progress" 
+        $exportStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink
+        while ($exportStatus.Status -ne 'Succeeded')
+        {
+            start-sleep -s 5
+            $exportStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink
+            Write-Host "Backup of" $allDbsJsonSrc.Databases.$sdb.dbname "is in progress" 
+        }
 
 
-    $databasecopy = New-AzureRmSqlDatabaseCopy -ResourceGroupName $allDbsJson.Databases.$sdb.resourcegroup -ServerName $allDbsJson.Databases.$sdb.server `
-                        -DatabaseName $allDbsJson.Databases.$sdb.dbname -CopyResourceGroupName $allDbsJson.Databases.$ddb.resourcegroup `
-                        -CopyServerName $allDbsJson.Databases.$ddb.server -CopyDatabaseName $allDbsJson.Databases.$ddb.dbname `
-                        -ServiceObjectiveName "S0"
+        Write-Host "Copying dump of " $allDbsJsonSrc.Databases.$sdb.dbname " to destination subscription" 
+        
+        Select-AzureRmSubscription -SubscriptionID $allDbsJson.SubscriptionId
+        
+        $importRequest = New-AzureRmSqlDatabaseImport -ResourceGroupName $allDbsJson.Databases.$ddb.resourcegroup -ServerName $allDbsJson.Databases.$ddb.server `
+                         -DatabaseName $allDbsJson.Databases.$ddb.dbname -StorageKeyType $allDbsJsonSrc.StorageKeytype -StorageKey $allDbsJsonSrc.StorageKey `
+                         -AdministratorLogin $allDbsJson.Server_credentials.($allDbsJson.Databases.$ddb.server).username `
+                         -AdministratorLoginPassword  ($allDbsJson.Server_credentials.($allDbsJson.Databases.$ddb.server).password | ConvertTo-SecureString -AsPlainText -Force )`
+                         -StorageUri $BacpacUriSrc –Edition Standard –ServiceObjectiveName S0 -DatabaseMaxSizeBytes 200000000
+
+        [int]$impStatusctr = 0
+        $impStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
+        while ($impStatus.Status -ne 'Succeeded')
+        {
+            Write-Host "Backup of " $($allDbsJson.Databases.$ddb.dbname) "is in progress" 
+            start-sleep -Milliseconds 300
+            $impStatus = Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
+            
+        }
+ 
+        
+    }
+    else
+    {
+        $databasecopy = New-AzureRmSqlDatabaseCopy -ResourceGroupName $allDbsJson.Databases.$sdb.resourcegroup -ServerName $allDbsJson.Databases.$sdb.server `
+                            -DatabaseName $allDbsJson.Databases.$sdb.dbname -CopyResourceGroupName $allDbsJson.Databases.$ddb.resourcegroup `
+                            -CopyServerName $allDbsJson.Databases.$ddb.server -CopyDatabaseName $allDbsJson.Databases.$ddb.dbname `
+                            -ServiceObjectiveName "S0"
+    }
 
     start-sleep -s 1
     # Adding to the respective elastic pool
