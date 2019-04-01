8ca02a9b236ce15887c7358092c5137367c7fec1
Ammar Sajid <ammar.sajid@techlogix.com>
2019-04-01 14:04:15 +0500

    Script to change data disk size of an Azure VM

---
+++ powershell/VM_change_data_disk_size.ps1
@@ -0,0 +1,20 @@
+﻿$subscriptionId = "cadxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx9"
+$DBserverName = "mytestsqlserver"
+$resourceGroupName = "TESTRS" 
+$newDiskSize = "200" # in Gigabytes
+$vmName = "test-VM"
+
+Login-AzureRmAccount
+ 
+# Select Azure Subscription
+Select-AzureRmSubscription -SubscriptionID $subscriptionId
+
+$vm = Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
+ 
+Stop-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
+
+$vm.StorageProfile.DataDisks[0].DiskSizeGB = $newDiskSize
+ 
+Update-AzureRmVM -ResourceGroupName $resourceGroupName -VM $vm
+ 
+Start-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
