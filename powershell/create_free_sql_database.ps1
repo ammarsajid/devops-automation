1e028e17b8e13242db0e1db5ff8c9bd57cc9dac8
Ammar Sajid <ammar.sajid@techlogix.com>
2019-04-29 10:29:53 +0500

    Script to create a FREE sql database

---
+++ powershell/create_free_sql_database.ps1
@@ -0,0 +1,21 @@
+﻿$subscriptionID = "cadxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx9"
+$DBserverName = "mytestsqlserver"
+$databasename = "mytestdatabase"
+$resourceGroup = "TESTRS" 
+$edition = "Free"
+
+# To log in to Azure Resource Manager
+Login-AzureRmAccount
+
+# Select a subscription to work with
+Select-AzureRmSubscription -SubscriptionID $subscriptionID
+
+
+#New-AzureRmSqlServer -ResourceGroupName $resourcegroupname `
+#    -ServerName $servername `
+#    -Location $location `
+#    -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminlogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+
+# Create a database
+New-AzureRmSqlDatabase  -ResourceGroupName $resourceGroup -ServerName $DBserverName -DatabaseName $databasename -Edition $edition
